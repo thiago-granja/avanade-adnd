338f0ba28b25e786d9c6f3b8efa4beffc8659bd7
Thiago Granja <thiagoaagranja@outlook.com>
2023-09-09 01:07:05 -0300

    first commit

---
+++ app/src/main/java/com/avanade/adnd/AdndApplication.java
@@ -0,0 +1,13 @@
+package com.avanade.adnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdndApplication.class, args);
+	}
+
+}
