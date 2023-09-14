7f8da95c077feda345aa7b4ceef8986c7a38f66d
Thiago Granja <thiagoaagranja@outlook.com>
2023-09-14 02:24:17 -0300

    add queries to battleParticipant and character repositories

--- app/src/main/java/com/avanade/adnd/repositories/BattleParticipantRepository.java
+++ app/src/main/java/com/avanade/adnd/repositories/BattleParticipantRepository.java
@@ -1,9 +1,17 @@
 package com.avanade.adnd.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.avanade.adnd.entities.Battle;
 import com.avanade.adnd.entities.BattleParticipant;
 
 public interface BattleParticipantRepository extends JpaRepository<BattleParticipant, Long> {
-    
+
+    @Query("SELECT bp FROM BattleParticipant bp WHERE bp.battle = :battle AND bp.hp > 0 ORDER BY bp.initiative DESC")
+    List<BattleParticipant> findActiveParticipants(@Param("battle") Battle battle);
 }
+
--- app/src/main/java/com/avanade/adnd/repositories/CharacterRepository.java
+++ app/src/main/java/com/avanade/adnd/repositories/CharacterRepository.java
@@ -1,10 +1,13 @@
 package com.avanade.adnd.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.avanade.adnd.entities.Character;
 
 public interface CharacterRepository extends JpaRepository<Character, Long> {
-
+    Optional<Character> findByName(String name);
+    
 }
 
