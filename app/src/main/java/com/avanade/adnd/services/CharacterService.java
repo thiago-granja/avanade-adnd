8bca0d21b08ffffc17e2b9ea2dc7f583dcc4328c
Thiago Granja <thiagoaagranja@outlook.com>
2023-09-14 02:31:56 -0300

    add mock character service, not working

--- app/src/main/java/com/avanade/adnd/services/CharacterService.java
+++ app/src/main/java/com/avanade/adnd/services/CharacterService.java
@@ -1,5 +1,11 @@
 package com.avanade.adnd.services;
 
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,12 +14,28 @@ import com.avanade.adnd.entities.Character;
 
 @Service
 public class CharacterService {
-    //private int MAX_STATS = 30;
 
     @Autowired
     private CharacterRepository characterRepository;
 
-    public Character save(Character character) {
-        return characterRepository.save(character);
-    }
+    private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+/*     public Character registerCharacter(Character character) {
+            Set<ConstraintViolation<Character>> violations = validator.validate(character);
+            
+            if (!violations.isEmpty()) {
+                StringBuilder sb = new StringBuilder();
+                for (ConstraintViolation<Character> violation : violations) {
+                    sb.append(violation.getMessage()).append(". ");
+                }
+                throw new IllegalArgumentException(sb.toString());
+            }
+
+            if(characterRepository.findByName(character.getName()).isPresent()) {
+                throw new IllegalArgumentException("Um personagem com esse nome já existe");
+            }
+
+            return characterRepository.save(character);
+    } */
+
 }
