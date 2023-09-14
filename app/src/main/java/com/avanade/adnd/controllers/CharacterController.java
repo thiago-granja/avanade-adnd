565fdbc86331b6d5545578ac5f99067eb02062d0
Thiago Granja <thiagoaagranja@outlook.com>
2023-09-14 13:44:37 -0300

    feat: add character CRUD

--- app/src/main/java/com/avanade/adnd/controllers/CharacterController.java
+++ app/src/main/java/com/avanade/adnd/controllers/CharacterController.java
@@ -1,17 +1,27 @@
 package com.avanade.adnd.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.avanade.adnd.services.CharacterService;
+
+import jakarta.persistence.EntityNotFoundException;
+
 import com.avanade.adnd.entities.Character;
 
 @Controller
@@ -21,11 +31,55 @@ public class CharacterController {
     @Autowired
     private CharacterService characterService;
 
-/*     @PostMapping
-    public ResponseEntity<Character> registerCharacter(@Validated @RequestBody Character character) {
-        Character savedCharacter = characterService.registerCharacter(character);
-        return new ResponseEntity<>(savedCharacter, HttpStatus.CREATED);
-    } */
+    @GetMapping
+    public List<Character> getAllCharacters() {
+        return characterService.getAllCharacters();
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<Character> getCharacterByName(@PathVariable String name) {
+        Optional<Character> characterOpt = characterService.getCharacterByName(name);
+        if(characterOpt.isPresent()) {
+            return ResponseEntity.ok(characterOpt.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public Character createCharacter(@RequestBody Character character) {
+        return characterService.createCharacter(character);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Character> updateCharacter(@PathVariable Long id, @RequestBody Character character) {
+        if (!id.equals(character.getId())) {
+            return ResponseEntity.badRequest().build();
+        }
+        try {
+            return ResponseEntity.ok(characterService.updateCharacter(character));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCharacter(@PathVariable Long id) {
+        characterService.deleteCharacter(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Character> patchCharacter(@PathVariable Long id, @RequestBody Character character) {
+        if (!id.equals(character.getId())) {
+            return ResponseEntity.badRequest().build();
+        }
+        try {
+            return ResponseEntity.ok(characterService.updateCharacter(character));
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
@@ -36,5 +90,10 @@ public class CharacterController {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFound(EntityNotFoundException e) {
+        return ResponseEntity.notFound().build();
     }
 }
--- app/src/main/java/com/avanade/adnd/services/CharacterService.java
+++ app/src/main/java/com/avanade/adnd/services/CharacterService.java
@@ -1,10 +1,17 @@
 package com.avanade.adnd.services;
 
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.avanade.adnd.repositories.CharacterRepository;
+import com.avanade.adnd.entities.Character;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CharacterService {
@@ -12,4 +19,46 @@ public class CharacterService {
     @Autowired
     private CharacterRepository characterRepository;
 
+    public List<Character> getAllCharacters() {
+        return characterRepository.findAll();
+    }
+
+    public Optional<Character> getCharacterById(Long id) {
+        return characterRepository.findById(id);
+    }
+
+    @Transactional
+    public Character createCharacter(Character character) {
+        if(characterRepository.findByName(character.getName()).isPresent()) {
+            throw new IllegalArgumentException("Personagem com nome " + character.getName() + " já existe");
+        }
+        return characterRepository.save(character);
+    }
+
+    @Transactional
+    public Character updateCharacter(Character character) {
+        if(!characterRepository.existsById(character.getId())) {
+            throw new EntityNotFoundException("Personagem com ID " + character.getId() + " não encontrado");
+        }
+        return characterRepository.save(character);
+    }
+
+    public Optional<Character> getCharacterByName(String name) {
+        return characterRepository.findByName(name);
+    }
+
+    @Transactional
+    public void deleteCharacter(Long id) {
+        Optional<Character> characterOptional = characterRepository.findById(id);
+
+        if (!characterOptional.isPresent()) {
+            throw new EntityNotFoundException("Personagem com ID " + id + " não encontrado");
+        }
+
+        try {
+            characterRepository.deleteById(id);
+        } catch (DataIntegrityViolationException ex) {
+            throw new IllegalStateException("Não é possível deletar o personagem pois ele tem registros relacionados a ele.");
+        }
+    }
 }
