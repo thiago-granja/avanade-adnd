834874d2067301951257ec865098706848834f4b
Thiago Granja <thiagoaagranja@outlook.com>
2023-09-14 02:43:25 -0300

    feat: add combat roll services on battlestep service

--- app/src/main/java/com/avanade/adnd/services/BattleStepService.java
+++ app/src/main/java/com/avanade/adnd/services/BattleStepService.java
@@ -1,9 +1,16 @@
 package com.avanade.adnd.services;
 
+import com.avanade.adnd.dtos.BattleDTO;
+import com.avanade.adnd.dtos.RollResultDTO;
+import com.avanade.adnd.entities.BattleParticipant;
 import com.avanade.adnd.entities.BattleStep;
+import com.avanade.adnd.entities.Character;
 import com.avanade.adnd.repositories.BattleParticipantRepository;
 import com.avanade.adnd.repositories.BattleRepository;
 import com.avanade.adnd.repositories.BattleStepRepository;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,5 +35,117 @@ public class BattleStepService {
         return battleStepRepository.save(step);
     }
 
+        public RollResultDTO rollInitiative(BattleParticipant battleParticipant) {
+        Character character = battleParticipant.getCharacter();
+        
+        Integer[] rolls = diceRollerService.rollDice(1, 20);
+        Integer roll = rolls[0];
+        Integer total = roll;
+    
+        String message = "O " + character.getName() + " rolou [" + roll + "] para sua iniciativa.";
+    
+        return new RollResultDTO(Arrays.asList(roll), total, message);
+    }
+
+    public RollResultDTO rollAttack(BattleParticipant attacker) {
+        Character character = attacker.getCharacter();
+    
+        if (character == null) {
+            throw new RuntimeException("Personagem não encontrado");
+        }
+        
+        Integer[] rolls = diceRollerService.rollDice(1, 12);
+        Integer roll = rolls[0];
+    
+        Integer total = roll + character.getStrength() + character.getAgility();
+    
+        String message = "O " + character.getName() + " rolou [" + roll + "], somando " + total + " para seu ataque. ";
+    
+        return new RollResultDTO(Arrays.asList(roll), total, message);
+    }
+    
+    public RollResultDTO rollDefense(BattleParticipant defender) {
+        Character character = defender.getCharacter();
+    
+        if (character == null) {
+            throw new RuntimeException("Personagem não encontrado");
+        }
+        
+        Integer[] rolls = diceRollerService.rollDice(1, 12);
+        Integer roll = rolls[0];
+    
+        Integer total = roll + character.getDefense() + character.getAgility();
+    
+        String message = "O " + character.getName() + " rolou [" + roll + "], somando " + total + " para sua defesa. ";
+    
+        return new RollResultDTO(Arrays.asList(roll), total, message);
+    }
+
+    public BattleDTO settleInitiatives(BattleParticipant player, BattleParticipant computer) {
+        RollResultDTO playerRoll = rollInitiative(player);
+        RollResultDTO computerRoll = rollInitiative(computer);
+        
+        while (playerRoll.getTotal() == computerRoll.getTotal()) {
+            playerRoll = rollInitiative(player);
+            computerRoll = rollInitiative(computer);
+        }
+
+        player.setInitiative(playerRoll.getTotal());
+        computer.setInitiative(computerRoll.getTotal());
+
+        battleParticipantRepository.save(player);
+        battleParticipantRepository.save(computer);
+
+        String winner = ( playerRoll.getTotal() > computerRoll.getTotal() 
+                        ? player.getCharacter().getName() 
+                        : computer.getCharacter().getName());
+
+        BattleDTO message = new BattleDTO.Builder()
+                                    .roll(playerRoll.getRolls())
+                                    .rollMessage(playerRoll.getMessage())
+                                    .playerCharacter(player.getCharacter().getName())
+                                    .computerCharacter(computer.getCharacter().getName())
+                                    .message("O " + winner + "ataca primeiro!")
+                                    .build();
+
+        return message;
+    }
+
+    public RollResultDTO rollDamage(BattleParticipant attacker) {
+        Character character = attacker.getCharacter();
+    
+        if (character == null) {
+            throw new RuntimeException("Personagem não encontrado");
+        }
+    
+        Integer[] rollArray = diceRollerService.rollDice(character.getDiceQuantity(), character.getDiceFaces());
+        List<Integer> rolls = Arrays.asList(rollArray);
+    
+        Integer total = rolls.stream().mapToInt(Integer::intValue).sum();
+        total += character.getStrength();
+    
+        String rollsMessage = rolls.size() > 1 ? " rolou " + rolls : " rolou [" + rolls.get(0) + "]";
+        String message = "O " + character.getName() + rollsMessage + ", causando " + total + " pontos de dano em seu oponente!";
+    
+        return new RollResultDTO(rolls, total, message);
+    }
+
+    public void takeDamage(BattleParticipant defender, RollResultDTO damage) {
+        Character character = defender.getCharacter();
+
+        if (character == null) {
+            throw new RuntimeException("Personagem não encontrado");
+        }
+
+        int remainingHp = defender.getHp() - damage.getTotal();
+        if (remainingHp < 0) remainingHp = 0;
+        defender.setHp(remainingHp);
+        battleParticipantRepository.save(defender);
+    }
+    
+    public Boolean checkIfAttackHit(RollResultDTO attack, RollResultDTO defense) {
+        if (attack.getTotal() > defense.getTotal()) return true;
+        return false;
+    }
 
 }
