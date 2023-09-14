a77422b76d9ffe1cca57c3d03d4b0ba0d3d001ab
Thiago Granja <thiagoaagranja@outlook.com>
2023-09-14 02:32:33 -0300

    add mock character controller, not working

--- app/src/main/java/com/avanade/adnd/controllers/CharacterController.java
+++ app/src/main/java/com/avanade/adnd/controllers/CharacterController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +21,20 @@ public class CharacterController {
     @Autowired
     private CharacterService characterService;
 
-    @PostMapping("/new")
-    public ResponseEntity<Character> registerCharacter(@RequestBody Character character) {
-        Character savedCharacter = characterService.save(character);
+    @PostMapping
+    public ResponseEntity<Character> registerCharacter(@Validated @RequestBody Character character) {
+        Character savedCharacter = characterService.registerCharacter(character);
         return new ResponseEntity<>(savedCharacter, HttpStatus.CREATED);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+        String errorMsg = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        return new ResponseEntity<>(errorMsg, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
